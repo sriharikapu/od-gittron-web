b8db3d8cdf961316689e8c2c722cb33583088507
dekan <dekanbro@gmail.com>
2019-02-15 13:58:52 -0700

    add link to rinkeby etherscan

--- src/components/shared/loader/loader.js
+++ src/components/shared/loader/loader.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Loader() {
+  { txHash } = this.props
   return (
     <div className="Loader">
       <div className="Loader__Contents">
         <h1>Loading ...</h1>
-        <p>Check Metamask to see if there's a transaction to confirm.</p>
+        {txHash ? (
+            <Link to={`https://rinkeby.etherscan.io/tx/${txHash}`}>
+              <p>Check Metamask to see if there's a transaction to confirm.</p>
+              <p>{repo.repo}</p>
+            </Link>
+        ) : (
+          <p>Check Metamask to see if there's a transaction to confirm.</p>
+        )}
         <p>If you've already confirmed the transaction, then it is likely processing.</p>
       </div>
     </div>
